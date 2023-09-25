bfc35478f069c9d87a1dc915f1d971853cb8a479
Kate <neilsenk09@gmail.com>
2023-09-24 20:21:09 -0400

    finally got leaflet to load! yay

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,15 +1,22 @@
 "use client";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import * as L from "leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import styles from "../styles/map.module.css";
 
 export default function Map() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className={styles.map}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={[51.505, -0.09]}></Marker>
     </MapContainer>
